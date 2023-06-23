2c79de378e834efc75e993aa83a33e1906d0b1ed
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2023-06-23 19:40:02 -0400

    Update shared_controls.js

--- src/js/shared_controls.js
+++ src/js/shared_controls.js
@@ -226,7 +226,7 @@ $(".ability").bind("keyup change", function () {
 
 	var ability = $(this).closest(".poke-info").find(".ability").val();
 
-	var TOGGLE_ABILITIES = ['Flash Fire', 'Intimidate', 'Minus', 'Plus', 'Slow Start', 'Unburden', 'Stakeout', 'Quickstart', 'Faulty Photon', 'System Purge'];
+	var TOGGLE_ABILITIES = ['Flash Fire', 'Intimidate', 'Minus', 'Plus', 'Slow Start', 'Unburden', 'Stakeout', 'Quickstart', 'Faulty Photon', 'System Purge', 'Forest Fury'];
 
 	if (TOGGLE_ABILITIES.indexOf(ability) >= 0) {
 		$(this).closest(".poke-info").find(".abilityToggle").show();
